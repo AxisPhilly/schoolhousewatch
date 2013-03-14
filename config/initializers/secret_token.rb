91b13b935dc9a86bac2d2a4363e1f7d887fecbeb
Casey Thomas <cpthomas@gmail.com>
2013-03-14 12:16:35 -0400

    Add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolWatch::Application.config.secret_token = 'df74cf9781cd0457481ef30a8c061ae3938e096d63998f2c7250a79d8f93774bc52d06955faf1c71e82bf0b964bbc8c0637f1e0b3f2946b621c8a1adf8f7e3f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SchoolWatch::Application.config.session_store :cookie_store, key: '_school-watch_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SchoolWatch::Application.config.session_store :active_record_store
