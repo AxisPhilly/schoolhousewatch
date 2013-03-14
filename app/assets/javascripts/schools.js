e96da9a402d8fbcf976da452edaa6e6c24aa9c2f
Casey Thomas <cpthomas@gmail.com>
2013-03-14 16:32:37 -0400

    More mockup

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -1,3 +1,20 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+if (typeof app === 'undefined' || !app) {
+  var app = {};
+}
+
+app.map = {};
+
+app.map.run = function() {
+  // create a map in the "map" div, set the view to a given place and zoom
+  var map = L.map('map').setView([51.505, -0.09], 13);
+
+  // add an OpenStreetMap tile layer
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  // add a marker in the given location, attach some popup content to it and open the popup
+  L.marker([51.5, -0.09]).addTo(map)
+      .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+      .openPopup();
+};
