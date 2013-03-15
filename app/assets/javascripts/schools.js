b86572c283aa858ea08ac0f45c7919e60564f133
Casey Thomas <cpthomas@gmail.com>
2013-03-15 14:58:28 -0400

    Clean up maps. Add city limits

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -7,14 +7,28 @@ app.map = {};
 app.map.run = function() {
   wax.tilejson('http://a.tiles.mapbox.com/v3/axisphilly.map-fg6flibn.jsonp',
     function(tilejson) {
-      var map = new L.Map('map-div')
+      window.map = new L.Map('map-div')
         .addLayer(new wax.leaf.connector(tilejson))
-        .setView(new L.LatLng(39.98, -75.15), 11);
+        .setView(new L.LatLng(40.00210, -75.12039), 11);
 
       wax.leaf.interaction()
         .map(map)
         .tilejson(tilejson)
         .on('on', function(o) {});
+
+      //add city limits to map
+      $.ajax({
+        url: '/city_limits.json',
+        success: function(res) {
+          L.geoJson(res, {
+            style:  {
+              "color": "#ff7800",
+              "weight": 5,
+              "opacity": 0.65
+            }
+          }).addTo(map);
+        }
+      });
 
       //add points to map
       $.ajax({
