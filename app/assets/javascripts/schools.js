6a3aec37d1cff029217b15699810f9b3cd1ce4a5
Casey Thomas <cpthomas@gmail.com>
2013-03-15 11:51:03 -0400

    Better maps--change map tab name, make ie css conditional

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -7,7 +7,7 @@ app.map = {};
 app.map.run = function() {
   wax.tilejson('http://a.tiles.mapbox.com/v3/axisphilly.map-fg6flibn.jsonp',
     function(tilejson) {
-      var map = new L.Map('map')
+      var map = new L.Map('map-div')
         .addLayer(new wax.leaf.connector(tilejson))
         .setView(new L.LatLng(39.98, -75.15), 11);
 
@@ -21,8 +21,9 @@ app.map.run = function() {
         url: '/schools.json',
         success: function(res) {
           for(var i=0; i<res.length; i++) {
-            console.log(res[i].lng);
-            var point = new L.marker([res[i].lat, res[i].lng]).addTo(map);
+            var point = new L.marker([res[i].lat, res[i].lng])
+                  .bindPopup('<a href="/schools/' + res[i].slug + '">' + res[i].name + '</a>')
+                  .addTo(map);
           }
         }
       });
