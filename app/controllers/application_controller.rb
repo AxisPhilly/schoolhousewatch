a76f11f2a7e2ba194b184a2ed1a444269574e961
Casey Thomas <cpthomas@gmail.com>
2013-03-15 14:18:53 -0400

    Move gsheet_to_hash functionality to helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,42 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  include ApplicationHelper
+
   def get_all_news
     gsession = GoogleDrive.login(ENV["GOOGLE_DRIVE_EMAIL"], ENV["GOOGLE_DRIVE_PASSWORD"])
     sheet = gsession.spreadsheet_by_key("0AsiZZJ_cOXiMdEhTVnNGblVYTDBBU1BpQkVxQ09vdVE").worksheets[0]
 
-    headers = sheet.rows[0]
-
-    stories = sheet.rows[1, sheet.rows.length].map { |row|
-      {
-        headers[0] => row[0],
-        headers[1] => row[1],
-        headers[2] => row[2],
-        headers[3] => row[3],
-        headers[4] => row[4]
-      }
-    }
-
-    stories
+    stories = gsheet_to_hash(sheet)
   end
 
   def get_all_resources
     gsession = GoogleDrive.login(ENV["GOOGLE_DRIVE_EMAIL"], ENV["GOOGLE_DRIVE_PASSWORD"])
     sheet = gsession.spreadsheet_by_key("0AsiZZJ_cOXiMdEhTVnNGblVYTDBBU1BpQkVxQ09vdVE").worksheets[1]
     
-    headers = sheet.rows[0]
-
-    resources = sheet.rows[1, sheet.rows.length].map { |row|
-      {
-        headers[0] => row[0],
-        headers[1] => row[1],
-        headers[2] => row[2],
-        headers[3] => row[3],
-        headers[4] => row[4]
-      }
-    }
-
-    resources
+    resources = gsheet_to_hash(sheet)
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def gsheet_to_hash(sheet)
+
+    headers = sheet.rows[0]
+
+    items = sheet.rows[1, sheet.rows.length].map { |row|
+      #date = Date.strptime(row[3], '%m/%d/%Y')
+
+      {
+        headers[0] => row[0],
+        headers[1] => row[1],
+        headers[2] => row[2],
+        headers[3] => row[3],
+        headers[4] => row[4]
+      }
+    }
+
+    items
+
+  end
+
 end
