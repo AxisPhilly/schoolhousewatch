7a3db8e5043e1e1de1670764a910c609308d92e8
Casey Thomas <cpthomas@gmail.com>
2013-03-15 11:48:58 -0400

    Pull homepage stories from googledoc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def get_all_news
+    gsession = GoogleDrive.login(ENV["GOOGLE_DRIVE_EMAIL"], ENV["GOOGLE_DRIVE_PASSWORD"])
+    sheet = gsession.spreadsheet_by_key("0AsiZZJ_cOXiMdEhTVnNGblVYTDBBU1BpQkVxQ09vdVE").worksheets[0]
+    sheet.rows[1, sheet.rows.length]
+  end
+
 end
--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -4,6 +4,7 @@ class SchoolsController < ApplicationController
   def index
     @schools = School.all
     @title = "School Watch"
+    @news = get_all_news
 
     respond_to do |format|
       format.html # index.html.erb
