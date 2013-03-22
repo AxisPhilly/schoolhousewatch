91f1c15fa7d0d24688a87ea43209db05f2f9d6ff
Casey Thomas <cpthomas@gmail.com>
2013-03-22 10:45:31 -0400

    Parse custom feed elements with Feedzirra

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Parse custom feed elements for external stories feed
+Feedzirra::Feed.add_common_feed_entry_element("source", :as => :source)
+Feedzirra::Feed.add_common_feed_entry_element("source_url", :as => :source_url)
+
 # Initialize the rails application
 SchoolWatch::Application.initialize!
