065343ae86d96c0f2ce9c2bf3fcfa3e627d0c219
Casey Thomas <cpthomas@gmail.com>
2013-04-04 13:45:14 -0400

    Set default sorting for schools model

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,6 +3,8 @@ class School < ActiveRecord::Base
   attr_accessible :address, :code, :lat, :level, :lng, :mkt_val_2014, 
   :name, :opa, :url, :slug, :flickr_image, :flickr_image_url, :context, :street_view
 
+  default_scope order('name ASC')
+
   def self.find(input)
     if input.to_i != 0
       super
