d1c7d218fa575553afe10b66aac822ae118600bd
Casey Thomas <cpthomas@gmail.com>
2013-04-25 16:35:07 -0400

    No more feedzirra.

--- config/initializers/feedzirra.rb
+++
@@ -1,3 +0,0 @@
-# Parse custom feed elements for external stories feed
-Feedzirra::Feed.add_common_feed_entry_element("source", :as => :source)
-Feedzirra::Feed.add_common_feed_entry_element("source_url", :as => :source_url)
